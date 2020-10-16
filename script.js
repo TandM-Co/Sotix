515983f6afb85b950de744306a8306379d0a3d72
AlexandrTiunchik <aleksandrtiunchik8s@gmail.com>
2020-10-16 21:01:07 +0300

    Added carousel

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const swiper = new Swiper('.swiper-container', {
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    renderBullet: function (index, className) {
-        return `<span class="circle circle__text mr-6 d-flex justify-content-center align-items-center">${index + 1}</span>`;
-    },
-  },
+$('.owl-carousel').owlCarousel({
+  items:1,
+  margin:10,
+  autoHeight:true
 });
